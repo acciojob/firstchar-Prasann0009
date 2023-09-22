ecadf126b477e586d568bdb469140fefbaaff33e
Prasann0009 <130238044+Prasann0009@users.noreply.github.com>
2023-09-22 17:25:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstChar(text) {
   // your code here
+	 const trimmedString = text.trim();
+
+  // Check if the trimmed string is empty
+  if (trimmedString === '') {
+    return '';
+  }
+
+  // Return the first character of the trimmed string
+  return trimmedString[0];
 }
 
 // Do not change the code below
 
 const text = prompt("Enter text:");
 alert(firstChar(text));
+
+
